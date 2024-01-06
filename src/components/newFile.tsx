f94e695470912c003da5c0e9fbdf9346732af595
Fuchs-Benjamin <wildvox67@gmail.com>
2024-01-06 09:08:18 +0100

    Site Done

---
+++ src/components/newFile.tsx
@@ -0,0 +1,36 @@
+<Fragment>
+<h2>
+<span class="first">{first}</span>
+<span class="second">{second}</span>
+</h2>
+<p>
+Our compony makes cool designs and branding. We make it with high quality.
+We have made cool companys and we are cool.
+</p>
+
+<style>{`
+    * {
+        background-color: #ff6969;
+        min-height: 100vh;
+    }
+    .first {
+        position: relative;
+        color: white;
+        font-size: 30px;
+        margin-left: 40px;
+        z-index: 1;
+        background: none;
+    }
+    .second {
+        position: relative;
+        display: flex;
+        top: -40px;
+        margin-left: 40px;
+        font-size: 70px;
+        font-weight: 750;
+        filter: opacity(0.3);
+        background: none;
+    }
+`}</style>
+
+</Fragment>;
